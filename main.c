a81ee76962adef25915d37322d3cc79ff5b4d86b
DavidNoeGar <david.noe@students.salle.url.edu>
2018-10-07 19:33:03 +0200

    Implemented principal function of the main that shows the basic functions of the list to the user

--- main.c
+++ main.c
@@ -32,3 +32,52 @@ void mostrar(LlistaBidOr l) {
 		printf("La lista esta vacia o estas en el final.\n");
 	}
 }
+void mostrarMenu(LlistaBidOr *l) {
+
+	printf("\tAñadimos elementos (Añadimos del 1 al 10)\n");
+	anadirElemento(l);
+	printf("\tAvanzamos\n");
+	if (LLISTABIDOR_final(*l)) {
+		printf("No podemos avanzar, estamos en el final.\n");
+	} else {
+		LLISTABIDOR_avanca(l);
+		printf("¡Hemos avanzado!\n");
+	}
+	printf("\tQuitamos elemento\n");
+	quitarElemento(l);
+	printf("\tMostramos elemento actual\n");
+	mostrar(*l);
+	printf("\tAvanzamos\n");
+	if (LLISTABIDOR_final(*l)) {
+		printf("No podemos avanzar, estamos en el final.\n");
+	} else {
+		LLISTABIDOR_avanca(l);
+		printf("¡Hemos avanzado!\n");
+	}
+	printf("\tVamos inicio\n");
+	LLISTABIDOR_vesInici(l);
+	printf("¡Estem a l'inici!\n");
+	printf("\t¿Está Vacia?\n");
+	if (LLISTABIDOR_buida(*l)) {
+		printf("¡La lista esta vacia!\n");
+	}
+	else {
+		printf("¡La lista NO esta vacia!\n");
+	}
+	printf("\t¿Estamos en en final?\n");
+	if (LLISTABIDOR_final(*l)) {
+		printf("¡La lista ha llegado al final!\n");
+	}
+	else {
+		printf("¡La lista aun no ha llegado al final!\n");
+	}
+	printf("\tQuitamos elemento\n");
+	quitarElemento(l);
+	printf("\tAvanzamos\n");
+	if (LLISTABIDOR_final(*l)) {
+		printf("No podemos avanzar, estamos en el final.\n");
+	} else {
+		LLISTABIDOR_avanca(l);
+		printf("¡Hemos avanzado!\n");
+	}
+}
