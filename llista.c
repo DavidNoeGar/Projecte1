83d8767c9741beb1e52f9520b94aa26cc0a45006
DavidNoeGar <david.noe@students.salle.url.edu>
2018-10-07 13:06:57 +0200

    Implementation of LLISTABIDOR_crea and LLISTABIDOR_insereixOrdenat

--- llista.c
+++ llista.c
@@ -1,6 +1,42 @@
 #include "llista.h"
 
-void LLISTABIDOR_insereixOrdenat (LlistaBidOr *l, int e) {}
+LlistaBidOr LLISTABIDOR_crea () {
+  LllistaBidOr l;
+  l.pri=(NodeBi*)malloc (sizeof(NodeBi));
+  if (l.pri==NULL) {
+    //Error
+  } else {
+    l.ult=(NodeBi*)malloc(sizeof(NodeBi));
+    if (l.ult==NULL) {
+       free(l.pri);
+       //Error
+    } else {
+       l.pri->e=ELEMENT_indefinit();
+       l.pri->ant=NULL;
+       l.ult->e=ELEMENT_indefinit();
+       l.ult->seg=NULL;
+       l.pri->seg=l.ult;
+       l.ult->ant=l.pri;
+       l.pdi=l.ult;
+    }
+  }
+  return l;
+}
+
+void LLISTABIDOR_insereixOrdenat (LlistaBidOr *l, int e) {
+  Node *aux;
+  aux=(Node*)malloc(sizeof(Node));
+  if (aux==NULL) {
+    //Error
+  } else {
+    aux->e=e;
+    l->ant=l->pri;
+    while (l->ant->seg!=NULL && e > l->ant->seg->e) {
+      l->ant=l->ant->seg;
+    }
+    aux->seg=l->ant->seg;
+    l->ant->seg=aux;
+  }}
 
 void LLITABIDOR_vesInici(LlistaBidOr *l) {}
 
