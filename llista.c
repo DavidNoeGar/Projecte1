0af9ed05660e79c87383c887b9eb0b686a3c76e6
DavidNoeGar <david.noe@students.salle.url.edu>
2018-10-07 12:30:33 +0200

    Creation of empty headers

---
+++ llista.c
@@ -0,0 +1,23 @@
+#include "llista.h"
+
+void LLISTABIDOR_insereixOrdenat () {}
+
+void LLITABIDOR_vesInici() {}
+
+void LLISTADOR_vesFinal() {}
+
+int LLISTABIDOR_inici() {}
+
+int LLISTABIDOR_final() {}
+
+void LLISTABIDOR_destrueixl() {}
+
+int LLISTABIDOR_consulta () {}
+
+void LLISTABIDOR_esborra() {}
+
+void LLISTABIDOR_avanca () {}
+
+void LLISTABIDOR_retocedeix() {}
+
+int LLISTABIDOR_buida () {}
---
+++ llista.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef	 _LLISTABI_H_
+#define _LLISTABI_H_
+#endif
+
+void LLISTABIDOR_insereixOrdenat ();
+int LLISTABIDOR_consulta ();
+void LLISTABIDOR_esborra ();
+void LLISTABIDOR_avanca ();
+void LLISTABIDOR_retocedeix ();
+int LLISTABIDOR_buida ();
+void LLISTABIDOR_vesInici ();
+void LLISTABIDOR_vesFinal ();
+int LLISTABIDOR_inici ();
+int LLISTABIDOR_final ();
+void LLISTABIDOR_destrueixl ();
